60565634e966c604e757d1b0149c423f50fd9071
Anish <josh.anish1@gmail.com>
2021-01-21 21:50:23 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -2,3 +2,7 @@
 a = [[a,c,v,c,d],[r,w,s,a,s],[100,22,1212,212]]
 merged = sum(a,[])
 
+#sorting pairs based on tuple indice
+tx = [(1,22),(32,5),(5,3),(8,1)]
+out = sorted(tx,key=lambda x:x[1])
+print(out) #[(8, 1), (5, 3), (32, 5), (1, 22)]
