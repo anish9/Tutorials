c367e3e05de8934ed57103e968d0999f91b7387a
Anish <josh.anish1@gmail.com>
2021-02-08 11:36:54 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -6,3 +6,8 @@ merged = sum(a,[])
 tx = [(1,22),(32,5),(5,3),(8,1)]
 out = sorted(tx,key=lambda x:x[1])
 print(out) #[(8, 1), (5, 3), (32, 5), (1, 22)]
+
+#dict usage 
+stage = 3
+shape =448
+grid_sizes = [shape//{0:16,1:32,2:48}[stage] for stage in range(3)]
